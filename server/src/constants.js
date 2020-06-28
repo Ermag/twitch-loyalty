d9b136a1b53cdccfe248a392d3d5adbf57fe9ccd
Ermag <ermag.bg@gmail.com>
2020-06-29 02:20:55 +0300

    Update constants.js

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,8 +1,8 @@
-const CLIENT_ID = 'cx4rlgwlppmertltfy1dql4twr2cw6'
+const CLIENT_ID = ''
 const APP_CONFIG = {
 	clientId: CLIENT_ID,
-	twitchSecret: 'jbWfPJaOHgKuQ5lWbYiG1ooRhhXhFajaFj44rEIumyU=',
-	twitchOwnerId: '183052295',
+	twitchSecret: '',
+	twitchOwnerId: '',
 	extensionURL: `https://www.twitch.tv/ext/${CLIENT_ID}-${process.env.npm_package_version}`
 }
 
